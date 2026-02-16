342b2c80a1a575d9f5aafc596e185bc231419c35
vishva patel <vishvapatel17325@gmail.com>
2026-02-16 18:03:16 +0530

    function call

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views #from (file)views import (fun)test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', views.test)
 ]
